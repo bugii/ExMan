31ea15df87bcb4b56f0c80c7128afb79a4c176dd
Dario Bugmann <dario.bugmann@outlook.com>
2020-10-20 21:24:07 +0200

    added logging for auto updates

--- sourceCode/src/electron/utils/updater.js
+++ sourceCode/src/electron/utils/updater.js
@@ -1,8 +1,8 @@
 const { autoUpdater } = require("electron-updater");
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
 
 module.exports = async () => {
   // check for updates (Github Releases)
-  console.log("Checking for updates");
-  const updateRes = await autoUpdater.checkForUpdatesAndNotify();
-  console.log(updateRes);
+  autoUpdater.checkForUpdatesAndNotify();
 };
