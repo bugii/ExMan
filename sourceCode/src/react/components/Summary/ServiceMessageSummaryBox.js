7072101f51ecd56ad93a28fbc04d9f66f71dc0f7
Taylor McCants <taylor.mccants@uzh.ch>
2020-11-01 14:24:46 +0100

    add "you missed x notifications" instead of msg summary

--- sourceCode/src/react/components/Summary/ServiceMessageSummaryBox.js
+++ sourceCode/src/react/components/Summary/ServiceMessageSummaryBox.js
@@ -16,37 +16,6 @@ export default function ServiceMessageSummaryBox(props) {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Table>
         {props.focusSession.services.map((service) =>
-          service.messages.length > 0 ? (
-            <tr
-              style={{
-                backgroundColor: props.backgroundColor,
-                borderRadius: 25,
-              }}
-            >
-              <td style={{ width: 150 }}>
-                <Service
-                  key={service.id}
-                  id={service.id}
-                  setActiveService={props.setActiveService}
-                  name={service.name}
-                  unreadCount={service.unreadCount}
-                  icon={props.offeredServices[service.name].icon}
-                  deleteApp={props.deleteApp}
-                />
-              </td>
-              {service.messages.sort(((a, b) => a.title !== b.title ? a.title < b.title ? -1 : 1 : 0)).map((message) => (
-                <tr style={{ fontSize: 12, textAlign: "left" }}>
-                  <td>{props.formatTime(message.timestamp)}</td>
-                  <td>{message.title}</td>
-                  <td>
-                    {service.name === "whatsapp"
-                      ? shortenMessage(message.body.slice(0, -9))
-                      : shortenMessage(message.body)}
-                  </td>
-                </tr>
-              ))}
-            </tr>
-          ) : (
             <tr
               style={{
                 backgroundColor: props.backgroundColor,
@@ -65,9 +34,8 @@ export default function ServiceMessageSummaryBox(props) {
                   showBubble={false}
                 />
               </td>
-              <td>No messages received.</td>
+              <td><b>{service.messages.length}</b> missed notifications from {service.name}.</td>
             </tr>
-          )
         )}
       </Table>
     </div>
