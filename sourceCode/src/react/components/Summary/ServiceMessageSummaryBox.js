8a15b7ce1345946997a65b9bb0a58a3ec0210910
Taylor McCants <taylor.mccants@uzh.ch>
2020-11-01 11:25:55 +0100

    sort messages by person

--- sourceCode/src/react/components/Summary/ServiceMessageSummaryBox.js
+++ sourceCode/src/react/components/Summary/ServiceMessageSummaryBox.js
@@ -34,7 +34,7 @@ export default function ServiceMessageSummaryBox(props) {
                   deleteApp={props.deleteApp}
                 />
               </td>
-              {service.messages.map((message) => (
+              {service.messages.sort(((a, b) => a.title !== b.title ? a.title < b.title ? -1 : 1 : 0)).map((message) => (
                 <tr style={{ fontSize: 12, textAlign: "left" }}>
                   <td>{props.formatTime(message.timestamp)}</td>
                   <td>{message.title}</td>
